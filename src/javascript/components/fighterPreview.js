017fc0281373666582e2b613cac4bc44a483ec08
Pootapss <77194102+Pootapss@users.noreply.github.com>
2021-01-18 00:01:29 +0300

    s

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,21 +7,47 @@ export function createFighterPreview(fighter, position) {
     className: `fighter-preview___root ${positionClassName}`,
   });
 
-  // todo: show fighter info (image, name, health, etc.)
+  function createProperty(keyValue) {
+    const nameElement = createElement({ tagName: 'span', className: 'fighter-preview___property' });
+    nameElement.innerText = keyValue.join(': ').replace(/(\w+):/, subStr => subStr.toUpperCase());
+  
+    return nameElement;
+  }
+
+  function createPreviewImage(source) {
+    const attributes = { src: source };
+    const imgElement = createElement({
+      tagName: 'img',
+      className: 'fighter-image___preview',
+      attributes
+    });
+
+    if(position === 'right') {
+      imgElement.style.transform = 'scale(-1, 1)';
+    }
+  
+    return imgElement;
+  }
+
+  if(fighter) {
+    const keyValueArrayOfObject = Object.entries(fighter);
+    fighterElement.append(createPreviewImage(fighter['source']));
+    keyValueArrayOfObject
+    .filter(keyValueAll => keyValueAll[0] !== '_id' && keyValueAll[0] !== 'source')
+    .forEach(keyValue => fighterElement.append(createProperty(keyValue)));
+  }
 
   return fighterElement;
 }
 
 export function createFighterImage(fighter) {
   const { source, name } = fighter;
-  const attributes = { 
-    src: source, 
-    title: name,
-    alt: name 
-  };
+  const attributes = { src: source };
   const imgElement = createElement({
     tagName: 'img',
     className: 'fighter-preview___img',
+    title: name,
+    alt: name,
     attributes,
   });
 
